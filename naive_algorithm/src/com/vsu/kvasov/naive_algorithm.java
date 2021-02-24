1ba1be1956f82697a805723898459aa7b10a37dd
kvasyan <kvasoff.an@gmail.com>
2021-02-24 10:46:46 +0300

    fix cycle text

--- naive_algorithm/src/com/vsu/kvasov/naive_algorithm.java
+++ naive_algorithm/src/com/vsu/kvasov/naive_algorithm.java
@@ -15,11 +15,12 @@ public class naive_algorithm {
     static ArrayList<Integer> searchNaive(String text, String sample) {
         ArrayList<Integer> foundPositions = new ArrayList<>();
         //по всем символам текста
-        for (int i = 0; i < text.length(); i++) {
+        for (int i = 0; i < text.length() - sample.length() + 1; i++) {
             int j = 0; //переменная для указания позиции образца
-            while (j < sample.length() && i + j < text.length() && sample.charAt(j) == text.charAt(i + j)) {
+            while (j < sample.length() && sample.charAt(j) == text.charAt(i + j)) {
                 j++;
             }
+
             if (j == sample.length()) {
                 foundPositions.add(i);
             }
